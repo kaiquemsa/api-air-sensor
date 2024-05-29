53d77892ba562b2e1d69c35ab3b6234af5731d33
Dev Kaique Silva <kaiquemeira@hotmail.com>
2024-05-29 10:24:35 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,18 @@ const cors = require('cors');
 import { AppDataSource } from "./database/data-source";
 import routes from "./routes";
 
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+app.use(routes);
+
 AppDataSource.initialize()
-  .then(async () => {
-    const app = express();
-    app.use(express.json());
-    app.use(cors({
-        origin: '*'
-        }
-    ));
-
-    app.use(routes);
-
-    app.listen(8000);
-
-    console.log("listening on port 8000");
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("listening on port 8000");
+    });
   })
   .catch((error) => console.log(error));
+
+export default app;
